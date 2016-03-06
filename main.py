4171179fd42c41b91ce86c2b46c2f36d6b28e3d2
vijaykyr <reddyv@reddyv-macbookair.roam.corp.google.com>
2016-03-06 10:35:00 -0500

    Parametrized sample rate

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from apiclient.discovery import build
 #   without having to write to disk first
 #   2) Can you batch multiple images into a single API request to reduce latency?
 
-def main(video_file,APIKey):
+def main(video_file, sample_rate, APIKey):
   #obtain service handle for vision API using API Key
   service = build('vision', 'v1', 
   discoveryServiceUrl='https://vision.googleapis.com/$discovery/rest?version=v1',
@@ -35,25 +35,25 @@ def main(video_file,APIKey):
     #do stuff with image
     cv2.imwrite("temp.jpg", image)     # save frame as JPEG file
     with open("temp.jpg", 'rb') as image:
-        image = base64.b64encode(image.read())  
-        service_request = service.images().annotate(
-          body={
-            'requests': [{
-              'image': {
-                'content': image
-               },
-              'features': [{
-                'type': 'LABEL_DETECTION',
-                'maxResults': 1,
-               }]
+      image = base64.b64encode(image.read())  
+      service_request = service.images().annotate(
+        body={
+          'requests': [{
+            'image': {
+              'content': image
+             },
+            'features': [{
+              'type': 'LABEL_DETECTION',
+              'maxResults': 1,
              }]
-          })
-        response = service_request.execute()
-        print(response)
-    
+           }]
+        })
+      response = service_request.execute()
+      print(response)
+  
     #advance to next image
     frame = frame + 1
-    position = position+1000
+    position = position+1000*sample_rate
     vidcap.set(0,position)
     success,image = vidcap.read()
   
@@ -61,10 +61,16 @@ def main(video_file,APIKey):
   os.remove("temp.jpg")
   
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser(description='Feed a video to the Google Vision API')
+  parser = argparse.ArgumentParser(
+    description='Feed a video to the Google Vision API')
   parser.add_argument(
     'video_file', help='The video you\'d like to process.')
   parser.add_argument(
-    'APIKey', help='The API Key that identifies your Google Cloud Console Project with Vision API Enabled')
+    'APIKey', help=('The API Key that identifies your Google Cloud Console '
+    'Project with Vision API Enabled'))
+  parser.add_argument(
+    '-s','--sample-rate',dest='samplerate', default=5, choices=range(1,61),
+    type=int, help=('The rate at which stills should be sampled from the '
+    'video. Default is 5 (one frame per 5 seconds).'))
   args = parser.parse_args()
-  main(args.video_file,args.APIKey)
+  main(args.video_file,args.samplerate,args.APIKey)
